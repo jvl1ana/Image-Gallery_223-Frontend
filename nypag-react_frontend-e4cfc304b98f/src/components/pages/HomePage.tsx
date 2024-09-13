4175aa2cbda66ad52f4fc45b70323f102c4bbae9
Fabiano Marino <fabiano.marino@noseryoung.com>
2024-09-13 14:24:38 +0200

    Add create post navigation and add create button

--- nypag-react_frontend-e4cfc304b98f/src/components/pages/HomePage.tsx
+++ nypag-react_frontend-e4cfc304b98f/src/components/pages/HomePage.tsx
@@ -1,23 +1,40 @@
-import { Box, flexbox } from '@mui/system';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import logo from '../../logo1.png';
-import LoginDialog from "./LoginDialog/LoginDialog";
+import LoginDialog from './LoginDialog/LoginDialog';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleCreatePost = () => {
+    navigate('/create-post');
+  };
+
   return (
-    <Box
-      display='flex'
-      alignItems='center'
-      justifyContent='center'
-      flexDirection={'column'}
-    >
-      <h1>Welcome to the Homepage</h1>
-      <LoginDialog />
-      <img
-        src={logo}
-        style={{ filter: 'invert(100%)' }}
-        className='App-logo'
-        alt='logo'
-      />
-    </Box>
+      <Box
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+          flexDirection={'column'}
+      >
+        <h1>Welcome to the Homepage</h1>
+        <LoginDialog />
+
+        <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCreatePost}
+            style={{ position: 'absolute', top: 20, right: 20 }}
+        >
+          Create Post
+        </Button>
+
+        <img
+            src={logo}
+            style={{ filter: 'invert(100%)' }}
+            className='App-logo'
+            alt='logo'
+        />
+      </Box>
   );
 }
