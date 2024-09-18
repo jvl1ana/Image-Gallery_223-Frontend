1a274cb1f0db29712a2b78e5c763fd0271f76d3c
Fabiano Marino <fabiano.marino@noseryoung.com>
2024-09-18 13:50:17 +0200

    Add Create Post button in PostGallery component

--- nypag-react_frontend-e4cfc304b98f/src/components/organisms/PostGallery.tsx
+++ nypag-react_frontend-e4cfc304b98f/src/components/organisms/PostGallery.tsx
@@ -1,30 +1,55 @@
 import ImagePostSmall from "../molecules/ImagePost/ImagePostSmall";
 import items from "../molecules/ImagePost/TestData";
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function PostGallery() {
+    const navigate = useNavigate();
+
+    const handleCreatePostClick = () => {
+        navigate('/create-post');
+    };
+
     return (
-        <Box padding={5} sx={{
-            columnWidth: '250px',
-            columnGap: '20px',
-        }}>
-            {items.map((item, index) => (
-                <Box
-                    key={index}
-                    sx={{
-                        breakInside: 'avoid',
-                        marginBottom: '20px',
-                    }}
-                >
-                    <ImagePostSmall
-                        UserProfilePicture={item.userProfilePhoto}
-                        Description={item.description}
-                        PostImage={item.postImage}
-                    />
-                </Box>
-            ))}
+        <Box padding={5} sx={{ position: 'relative' }}>
+
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleCreatePostClick}
+                sx={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px',
+                    zIndex: 1,
+                }}
+            >
+                Create Post
+            </Button>
+
+
+            <Box
+                sx={{
+                    columnWidth: '250px',
+                    columnGap: '20px',
+                }}
+            >
+                {items.map((item, index) => (
+                    <Box
+                        key={index}
+                        sx={{
+                            breakInside: 'avoid',
+                            marginBottom: '20px',
+                        }}
+                    >
+                        <ImagePostSmall
+                            UserProfilePicture={item.userProfilePhoto}
+                            Description={item.description}
+                            PostImage={item.postImage}
+                        />
+                    </Box>
+                ))}
+            </Box>
         </Box>
     );
 }
-
-
