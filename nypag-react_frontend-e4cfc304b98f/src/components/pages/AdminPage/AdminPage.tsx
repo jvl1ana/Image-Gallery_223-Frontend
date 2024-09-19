0efac6fa394b224dd2760295b573f0d8c0fd18e4
Fabiano Marino <fabiano.marino@noseryoung.com>
2024-09-19 16:02:05 +0200

    Create Admin page

---
+++ nypag-react_frontend-e4cfc304b98f/src/components/pages/AdminPage/AdminPage.tsx
@@ -0,0 +1,10 @@
+// src/components/pages/AdminPage/AdminPage.tsx
+import React from 'react';
+
+export default function AdminPage() {
+    return (
+        <div>
+            <h1>Admin</h1>
+        </div>
+    );
+}
